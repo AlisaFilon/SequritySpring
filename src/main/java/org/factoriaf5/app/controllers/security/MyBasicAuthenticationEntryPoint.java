a5d95f6ce49686c9afb75825fc1db41496a850dd
yassscoder <yasminlcg@gmail.com>
2021-08-30 09:42:55 +0200

    cambios mybasicathenticationentrypoint para que compile

--- src/main/java/org/factoriaf5/app/controllers/security/MyBasicAuthenticationEntryPoint.java
+++ src/main/java/org/factoriaf5/app/controllers/security/MyBasicAuthenticationEntryPoint.java
@@ -2,7 +2,6 @@ package org.factoriaf5.app.controllers.security;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -14,15 +13,15 @@ import java.io.PrintWriter;
         @Override
         public void commence(
                 HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
-                throws IOException, ServletException {
-            response.addHeader("WWW-Authenticate", "Basic realm="" + getRealmName() + """);
+                throws IOException{
+            response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName() + "");
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             PrintWriter writer = response.getWriter();
             writer.println("HTTP Status 401 - " + authEx.getMessage());
         }
 
         @Override
-        public void afterPropertiesSet() throws Exception {
+        public void afterPropertiesSet()  {
             setRealmName("Baeldung");
             super.afterPropertiesSet();
         }
